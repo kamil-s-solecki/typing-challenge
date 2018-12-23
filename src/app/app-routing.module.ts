ab38694d734774f13d3a64b48f9a0b880c4d0f3f
Kamil Solecki <kamsolecki@gmail.com>
2018-12-23 22:54:40 +0100

    Add route for results page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ResultsComponent } from './results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'results', component: ResultsComponent },
+  { path: '**', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
