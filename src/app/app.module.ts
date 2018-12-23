a075b714d8e8e4e2a889b1a3b59aef0e7db5c8f2
Kamil Solecki <kamsolecki@gmail.com>
2018-12-23 13:32:29 +0100

    Generate text display component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextDisplayComponent } from './text-display/text-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextDisplayComponent
   ],
   imports: [
     BrowserModule,
