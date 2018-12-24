8feb3585d8defa18b65a6a68659595216db8d8bf
Kamil Solecki <kamsolecki@gmail.com>
2018-12-24 10:48:31 +0100

    Add sample texts

--- src/app/__data/texts.ts
+++ src/app/__data/texts.ts
@@ -6,7 +6,51 @@ export const texts: Text[] = [
     "from www.lipsum.com"   
   ),
   text(
+    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.",
+    "from www.lipsum.com"   
+  ),
+  text(
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "from 'The Extremes of Good and Evil' by Cicero"
+  ),
+  text(
+    "He who fights with monsters should look to it that he himself does not become a monster. And when you gaze long into an abyss the abyss also gazes into you.",
+    "from 'Beyond Good and Evil' by Friedrich Nietzsche"
+  ),
+  text(
+    "Physiologists should think twice before positioning the drive for self-preservation as the cardinal drive of an organic being. Above all, a living thing wants to discharge its strength - life itself is will to power - self-preservation is only one of the indirect and most frequent consequences of this.",
+    "from 'Beyond Good and Evil' by Friedrich Nietzsche"
+  ),
+  text(
+    "How can we satisfy ourselves without going on in infinitum? And, after all, what satisfaction is there in that infinite progression? Let us remember the story of the Indian philosopher and his elephant. It was never more applicable than to the present subject. If the material world rests upon a similar ideal world, this ideal world must rest upon some other; and so on, without end. It were better, therefore, never to look beyond the present material world.",
+    "from 'Dialogues Concerning Natural Religion' by David Hume"
+  ),
+  text(
+    "All that belongs to human understanding, in this deep ignorance and obscurity, is to be sceptical, or at least cautious, and not to admit of any hypothesis whatever, much less of any which is supported by no appearance of probability.",
+    "from 'Dialogues Concerning Natural Religion' by David Hume"
+  ),
+  text(
+    "Let the errors and deceits of our very senses be set before us; the insuperable difficulties which attend first principles in all systems; the contradictions which adhere to the very ideas of matter, cause and effect, extension, space, time, motion; and, in a word, quantity of all kinds, the object of the only science that can fairly pretend to any certainty or evidence.",
+    "from 'Dialogues Concerning Natural Religion' by David Hume"
+  ),
+  text(
+    "Then before we began to see or hear or otherwise perceive, we must have possessed knowledge of the Equal itself if we were about to refer our sense perceptions of equal objects to it, and realized that all of them were eager to be like it, but were inferior.",
+    "Socrates in Plato's Phaedo"
+  ),
+  text(
+    "I believe, as perhaps you do, that precise knowledge on that subject is impossible or extremely difficult in our present life, but that it surely shows a very poor spirit not to examine thoroughly what is said about it, and to desist before one is exhausted by an all-round investigation.",
+    "Simmias in Plato's Phaedo"
+  ),
+  text(
+    "Begin the morning by saying to thyself, I shall meet with the busy-body, the ungrateful, arrogant, deceitful, envious, unsocial. All these things happen to them by reason of their ignorance of what is good and evil.",
+    "from 'The Meditations' by Marcus Aurelius"
+  ),
+  text(
+    "Remember how long thou hast been putting off these things, and how often thou hast received an opportunity from the gods, and yet dost not use it. Thou must now at last perceive of what universe thou art a part, and of what administrator of the universe thy existence is an efflux, and that a limit of time is fixed for thee, which if thou dost not use for clearing away the clouds from thy mind, it will go and thou wilt go, and it will never return.",
+    "from 'The Meditations' by Marcus Aurelius"
+  ),
+  text(
+    "Through not observing what is in the mind of another a man has seldom been seen to be unhappy; but those who do not observe the movements of their own minds must of necessity be unhappy.",
+    "from 'The Meditations' by Marcus Aurelius"
   ),
 ];
